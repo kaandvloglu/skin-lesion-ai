cfaa52f66ddbf872ae4ef9b20cedc96ea12bec90
kaan <kaandevelioglu02@gmail.com>
2026-09-08 00:22:35 +0300

    Add augment_image preprocessing function

--- ai/src/preprocessing.py
+++ ai/src/preprocessing.py
@@ -1,27 +1,17 @@
 import pandas as pd
 import tensorflow as tf
 
-def augment_image(image):
-    image = tf.image.random_flip_left_right(image)
-    image = tf.image.random_flip_up_down(image)
-    image = tf.image.random_brightness(image, 0.15)
-    image = tf.image.random_contrast(image, 0.8, 1.2)
-    return image
-
 IMG_SIZE = 300
 
 def preprocess_image(path):
-
     img = tf.io.read_file(str(path))
     img = tf.image.decode_jpeg(img, channels=3)
     img = tf.image.resize(img, (IMG_SIZE, IMG_SIZE))
     img = img / 255.0
-
     return img
 
 
 def encode_metadata(df, columns=None):
-
     metadata = df[
         [
             "age_approx",
@@ -32,10 +22,17 @@ def encode_metadata(df, columns=None):
     ].copy()
 
     metadata = pd.get_dummies(metadata)
-
     metadata = metadata.fillna(0)
 
     if columns is not None:
         metadata = metadata.reindex(columns=columns, fill_value=0)
 
     return metadata.astype("float32")
+
+
+def augment_image(image):
+    image = tf.image.random_flip_left_right(image)
+    image = tf.image.random_flip_up_down(image)
+    image = tf.image.random_brightness(image, 0.15)
+    image = tf.image.random_contrast(image, 0.8, 1.2)
+    return image
